c29b1b50a02e11998ca2991e6da76163c394b88f
Ankan <adas8682@gmail.com>
2025-04-13 00:40:30 +0530

    adding spring profile created dev, qa, prod

--- src/main/java/com/projectfor/jenkines/controller/TestController.java
+++ src/main/java/com/projectfor/jenkines/controller/TestController.java
@@ -2,16 +2,21 @@ package com.projectfor.jenkines.controller;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 	
+	 @Autowired
+	 private Environment env;
+	
 	@GetMapping("/getApp")
 	public String getAppDate()
 	{	
-		return "App is running !!";
+		return "App is running !!. The APP name is: "+env.getProperty("spring.application.name");
 	}
 	
 	@GetMapping("/getData")
